958e2ec1b190cb8249db55bda6ce697448ab8a12
blastedprints <137637091+blastedprints@users.noreply.github.com>
2024-07-23 07:45:21 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,12 @@
-# Etsy API Credentials (Replace with your actual credentials)
-ETSY_API_KEY = ''
-ETSY_SHOP_ID = ''
-
-# Email Configuration (Replace with your credentials and settings)
-SMTP_SERVER = 'smtp.gmail.com'
-SMTP_PORT = 587  # Or your SMTP server's port
-EMAIL_FROM = 'email@example.com'
-EMAIL_TO = 'email@example.com' # Or the recipient's email
-EMAIL_PASSWORD = 'password'
-MINIMUM_QUANTITY = 5
+class creds:
+    # Etsy API Credentials (Replace with your actual credentials)
+    ETSY_API_KEY = ''
+    ETSY_SHOP_ID = ''
+    
+    # Email Configuration (Replace with your credentials and settings)
+    SMTP_SERVER = 'smtp.gmail.com'
+    SMTP_PORT = 587  # Or your SMTP server's port
+    EMAIL_FROM = 'email@example.com'
+    EMAIL_TO = 'email@example.com' # Or the recipient's email
+    EMAIL_PASSWORD = 'password'
+    MINIMUM_QUANTITY = 5
